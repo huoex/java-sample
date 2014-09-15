83879250a7c34a85f9ceac577719c9ab7a7300d1
huoex <huoex0628@gmai.com>
2014-09-15 20:09:13 +0800

    future cancel
    Signed-off-by: huoex <huoex0628@gmai.com>

---
+++ java-sample/src/hex/com/test07/Thread/Future/Cancel/Task.java
@@ -0,0 +1,58 @@
+package hex.com.test07.Thread.Future.Cancel;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class Task implements Callable<String> {
+
+	/**
+	 * Main method of the task. It has an infiniteloop that writes a message to
+	 * the console every 100 milliseconds
+	 */
+	@Override
+	public String call() throws Exception {
+		while (true) {
+			System.out.printf("Task: Test\n");
+			Thread.sleep(100);
+		}
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Create an executor
+		ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors
+				.newCachedThreadPool();
+
+		// Create a task
+		Task task = new Task();
+
+		System.out.printf("Main: Executing the Task\n");
+
+		// Send the task to the executor
+		Future<String> result = executor.submit(task);
+
+		// Sleep during two seconds
+		try {
+			TimeUnit.SECONDS.sleep(2);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		// Cancel the task, finishing its execution
+		System.out.printf("Main: Cancelling the Task\n");
+		result.cancel(true);
+		// Verify that the task has been cancelled
+		System.out.printf("Main: Cancelled: %s\n", result.isCancelled());
+		System.out.printf("Main: Done: %s\n", result.isDone());
+
+		// Shutdown the executor
+		executor.shutdown();
+		System.out.printf("Main: The executor has finished\n");
+	}
+
+}
